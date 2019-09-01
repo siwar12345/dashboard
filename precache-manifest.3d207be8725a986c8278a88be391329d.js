764a48eeedbaa93f4feedae665ed03da9de7856f
unknown <siwrayadi2018@gmail.com>
2019-09-02 00:42:30 +0200

    Updates

---
+++ precache-manifest.3d207be8725a986c8278a88be391329d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a13eabaa97383acb297a77f6b3ff3aa",
+    "url": "/dashbord/static/media/trophy.9a13eaba.svg"
+  },
+  {
+    "revision": "048aef30a5eb8d10d0c1956354681a64",
+    "url": "/dashbord/static/media/star.048aef30.svg"
+  },
+  {
+    "revision": "549aa6e75c8b4f04d1f173d426f3ed1d",
+    "url": "/dashbord/static/media/rss.549aa6e7.svg"
+  },
+  {
+    "revision": "46deee71e3f78091e35e1e9979b4d658",
+    "url": "/dashbord/static/media/doller.46deee71.svg"
+  },
+  {
+    "revision": "03be703c1e56f5141d49",
+    "url": "/dashbord/static/js/runtime~main.03be703c.js"
+  },
+  {
+    "revision": "aa961d8726110ff81e4e",
+    "url": "/dashbord/static/js/main.aa961d87.chunk.js"
+  },
+  {
+    "revision": "975c8e0ace91cf05ad56",
+    "url": "/dashbord/static/js/1.975c8e0a.chunk.js"
+  },
+  {
+    "revision": "aa961d8726110ff81e4e",
+    "url": "/dashbord/static/css/main.3a8ca268.chunk.css"
+  },
+  {
+    "revision": "975c8e0ace91cf05ad56",
+    "url": "/dashbord/static/css/1.680e08ba.chunk.css"
+  },
+  {
+    "revision": "38ba817dfb7a9399898a042d6b628875",
+    "url": "/dashbord/index.html"
+  }
+];
